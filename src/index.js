302ffe254c78d0ad95a3098e9e8ca2ced1d35003
darwin74710 <darwin74710@gmail.com>
2025-05-13 05:11:55 -0500

    añadir base de datos postgress

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
-
 import indexRoutes from './routes/index.js'
 
 const app = express()
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,22 @@
 import { Router } from 'express'
-const router = Router()
+import {config} from 'dotenv'
+import pg from 'pg'
 
-router.get('/', (req, res) => res.render('index', {title: 'First web with Node'}))
+config()
+
+const router = Router()
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
+
+router.get('/', (req, res) =>res.render('index', {title: 'First web with Node'}))
 router.get('/about', (req, res) => res.render('about', {title: 'About Me'}))
 router.get('/contact', (req, res) => res.render('contact', {title: 'Contact Page'}))
+
+router.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+})
 
 export default router
